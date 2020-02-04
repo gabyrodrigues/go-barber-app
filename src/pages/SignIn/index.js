99638e90f7dd8abf0876eb5690884b3acb7e9029
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-02-04 12:32:14 -0300

    input e button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from './../../components/Input';
+import Button from './../../components/Button';
 import Background from './../../components/Background';
 
 export default function SignIn() {
-    return <Background><Text>SignIn</Text></Background>;
+    return (
+        <Background>
+            <Text>SignIn</Text>
 
+            <Input 
+                icon="call" 
+                placeholder="Digite seu nome" 
+            />
+
+            <Button>Entrar</Button>
+        </Background>
+    );
 }
