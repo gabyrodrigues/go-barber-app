33e841d959bb41db81fb3c27fea12a08953f79dd
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-02-05 11:20:30 -0300

    status bar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 };
