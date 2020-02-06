c854762d8233232e5ef9e759e98ca0eabf95a426
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-02-06 12:27:09 -0300

    sign in e sign up redux (incompleto)

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,27 @@
-import React, { useRef } from 'react'; //ref é para a acessibilidade
+import React, { useRef, useState } from 'react'; //ref é para a acessibilidade
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from './../../assets/logo.png';
 
 import Background from './../../components/Background';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import { Container, Form, FormInput, 
          SubmitButton, SignLink, SignLinkText 
         } from './styles';
 
 export default function SignIn({ navigation }) {
+    const dispatch = useDispatch();
     const passwordRef = useRef();
 
-    function handleSubmit() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit() {
+        dispatch(signInRequest(email, password));
     }
 
     return (
@@ -30,18 +38,23 @@ export default function SignIn({ navigation }) {
                         placeholder="Digite seu email"
                         returnKeyType="next" //quando abre o teclado a tecla de enviar muda pro icone/texto de next
                         onSubmitEditing={() => passwordRef.current.focus() }
+                        value={email}
+                        onChangeText={setEmail}
                     />
 
                     <FormInput 
                         icon="lock-outline"
                         secureTextEntry
                         placeholder="Sua senha secreta"
+                        autoCapitalize="none"
                         ref={passwordRef}
                         returnKeyType="send" //quando clicar no botão de "enter" ele envia (já que ele é o último input)
                         onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
                     />
 
-                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>Acessar</SubmitButton>
                 </Form>
 
                 <SignLink onPress={ () => navigation.navigate('SignUp') }>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,30 @@
-import React, { useRef } from 'react'; //useRef ajuda na acessibilidade
+import React, { useRef, useState } from 'react'; //useRef ajuda na acessibilidade
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from './../../assets/logo.png';
 
 import Background from './../../components/Background';
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 import { Container, Form, FormInput, 
          SubmitButton, SignLink, SignLinkText 
         } from './styles';
 
 export default function SignUp({ navigation }) {
+    const dispatch = useDispatch();
+
     const emailRef = useRef();
     const passwordRef = useRef();
 
-    function handleSubmit () {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit () {
+        dispatch(signUpRequest(name, email, password));
     }
 
     return (
@@ -27,9 +37,11 @@ export default function SignUp({ navigation }) {
                         icon="person-outline"
                         autoCorrect={false}
                         autoCapitalize="none"
-                        placeholder="Nome completo"
+                        placeholder="Digite seu nome"
                         returnKeyType="next" //quando abre o teclado a tecla de enviar muda pro icone/texto de next
-                        onSubmitEditing ={() => emailRef.current.focus()}
+                        onSubmitEditing={() => emailRef.current.focus() }
+                        value={name}
+                        onChangeText={setName}
                     />
 
                     <FormInput 
@@ -38,21 +50,26 @@ export default function SignUp({ navigation }) {
                         autoCorrect={false}
                         autoCapitalize="none"
                         placeholder="Digite seu email"
+                        returnKeyType="next" //quando abre o teclado a tecla de enviar muda pro icone/texto de next
+                        onSubmitEditing={() => passwordRef.current.focus() }
                         ref={emailRef}
-                        returnKeyType="next" 
-                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
                     />
 
                     <FormInput 
                         icon="lock-outline"
                         secureTextEntry
                         placeholder="Sua senha secreta"
+                        autoCapitalize="none"
                         ref={passwordRef}
-                        returnKeyType="send" //quando confirma no teclado já envia (sem precisar clicar no botão)
+                        returnKeyType="send" //quando clicar no botão de "enter" ele envia (já que ele é o último input)
                         onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
                     />
 
-                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>Criar conta</SubmitButton>
                 </Form>
 
                 <SignLink onPress={ () => navigation.navigate('SignIn') }>
