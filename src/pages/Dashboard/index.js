8ced7a5b22c1df8a34f7cd44ed62253aa27b005a
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-02-11 11:09:29 -0300

    listagem de agendamentos (buscando da api), formatação de datas e cancelamento de agendamento

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container, Title, List } from './styles';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import Background from '../../components/Background';
 import Appointment from '../../components/Appointment';
 
-const data = [1, 2, 3, 4, 5]; //listagem de agendamentos
-
 export default function Dashboard() {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        async function loadAppointments() {
+            const response = await api.get('appointments'); //busca de agendamentos na api
+
+            setAppointments(response.data);
+        }
+
+        loadAppointments();
+    }, []);
+
+    async function handleCancel(id) {
+        const response = await api.delete(`appointments/${id}`);
+
+        //busca o agendamento deletado (marcado como cancelado) e seta a propriedade canceled_at dentro dele pra sumir da listagen de agendamentos
+        setAppointments(
+            appointments.map(appointment =>
+                //se o id for igual ao id do cancelamento, então ele
+                //retorna todos os dados de dentro desse appointment
+                //e também atualiza o canceled_at
+                appointment.id === id
+                ? {
+                    ...appointment,
+                    canceled_at: response.data.canceled_at
+                }
+                : appointment
+            )
+        )
+    }
+
     return (
         <Background>
             <Container>
                 <Title>Agendamentos</Title>
                 <List 
-                 data={data}
-                 keyExtractor={item => String(item)}
+                 data={appointments}
+                 keyExtractor={item => String(item.id)}
                  renderItem={({ item }) => (
                     <Appointment 
+                     onCancel={() => handleCancel(item.id)}
                      data={item}
                     />
                  )}
